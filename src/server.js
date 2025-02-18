ac5a8bf74e24ff684cc50dd3be55ba305e6394e1
LEGION <LEGION@LAPTOP-OCCDFBEI>
2025-02-18 16:49:40 +0700

    hoàn thành chức năng tìm kiếm bài đăng theo tên sản phẩm

--- src/server.js
+++ src/server.js
@@ -3,21 +3,33 @@ const appConfig = require('./config/app.config');
 const Database = require('./database/mysql.database');
 const AuthRoutes = require('./routes/Auth.routes');
 const handleErrorsMiddeleware = require('./middlewares/error.middleware');
+const cors = require("cors");
+const postRoutes = require("./routes/post.routes");
+const errorMiddleware = require('./middlewares/error.middleware');
 
 const app = express();
 
-const PORT = appConfig.port;
+// Middleware
+app.use(cors()); // Hỗ trợ CORS
+app.use(express.json()); // Xử lý JSON request body
+app.use(express.urlencoded({ extended: true })); // Hỗ trợ xử lý form data
 
 Database.getInstance();
 
 app.use(express.json());
-app.use('/', AuthRoutes);
-
-const server = app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+app.use('/api', AuthRoutes);
+app.use("/api/posts", postRoutes);
 
 app.use("*", (req, res, next) => {
     next(new NotFoundRequestError());
 });
 app.use(handleErrorsMiddeleware);
+
+// Middleware xử lý lỗi
+app.use(errorMiddleware);
+
+// Khởi chạy server
+const PORT = appConfig.port || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
