d6f28e68f560abc724fac40e86b0db9e08a7e6a8
LEGION <LEGION@LAPTOP-OCCDFBEI>
2025-03-11 16:55:58 +0700

    .

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post("/", uploadMemory.array("images"), validate(postSchema), CatchAsync(postController.createPost));
 router.get("/all", postController.searchPosts);//API lọc bài đăng theo tên danh mục http://localhost:4000/api/post/all?location=New%20York&min_price=100&max_price=500000
 router.get("/newpost", postController.getNewPosts);
+router.get("/search-suggestions", postController.Suggestions);
 router.get("/:id", postController.getPostById);
 router.get("/:email", postController.getPostsByUserEmail);
 router.put('/update/:postId', postController.updatePost);
