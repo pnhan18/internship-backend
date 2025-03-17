cb4c133a2787e4ea3424fb7b97f536b22a935b64
LEGION <LEGION@LAPTOP-OCCDFBEI>
2025-03-17 13:03:34 +0700

    ..

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -6,15 +6,16 @@ const CatchAsync = require("../utils/CatchAsync");
 const postController = require("../controllers/post.controller");
 
 const router = express.Router();
-router.post("/", uploadMemory.array("images"), validate(postSchema), CatchAsync(postController.createPost));
+
 router.get("/all", postController.searchPosts);//API lọc bài đăng theo tên danh mục http://localhost:4000/api/post/all?location=New%20York&min_price=100&max_price=500000
 router.get("/newpost", postController.getNewPosts);
 router.get("/search-suggestions", postController.Suggestions);
-router.get("/:id", postController.getPostById);
-router.get("/:email", postController.getPostsByUserEmail);
+router.get("/detail/:id", postController.getPostById);
 router.put('/update/:postId', postController.updatePost);
 router.post('/addfavorite',postController.addFavorite);
+router.get("/:email", postController.getPostsByUserEmail);
 router.delete('/removefavorite', postController.removeFavorite);
 router.get('/getfavorite', postController.getFavoriteList);
+router.post("/", uploadMemory.array("images"), validate(postSchema), CatchAsync(postController.createPost));
 
 module.exports = router;
